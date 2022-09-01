b91498bec8869f1426deacbe737ae7579faef769
jcolejeff <ikwuhj@gmail.com>
2022-09-01 17:42:45 +0100

    finished register page setup

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,6 +1,7 @@
 import main from "../assets/images/main.svg";
 import Wrapper from "../assets/wrappers/LandingPage";
 import { Logo } from "../components";
+import { Link } from "react-router-dom";
 const Landing = () => {
 	return (
 		<Wrapper>
@@ -18,7 +19,9 @@ const Landing = () => {
 						est quas porro molestiae tenetur laboriosam ducimus veniam incidunt
 						dicta suscipit.
 					</p>
-					<button className="btn btn-hero">Login/Register</button>
+					<Link to="/register" className="btn btn-hero">
+						Login/Register
+					</Link>
 				</div>
 				<img src={main} alt="job hunt" className="img main-img" />
 			</div>
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,7 +1,49 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import Wrapper from "../assets/wrappers/RegisterPage";
+import { Logo } from "../components";
+
+const initialState = {
+	name: "",
+	email: "",
+	password: "",
+	isMember: true,
+};
 
 const Register = () => {
-	return <div>Register</div>;
+	const [values, setValues] = useState(initialState);
+
+	const handleChange = (event) => {
+		console.log(event);
+	};
+	const onSubmit = (event) => {
+		event.preventDefault();
+		console.log(event.target);
+	};
+	return (
+		<Wrapper className="full-page">
+			<form className="form" onSubmit={onSubmit}>
+				<Logo></Logo>
+				<h3>Login</h3>
+				{/* name field */}
+				<div className="form-row">
+					<label htmlFor="name" className="form-label">
+						name
+					</label>
+					<input
+						type="text"
+						value={values.name}
+						name="name"
+						onChange={handleChange}
+						className="form-input"
+					/>
+				</div>
+				<button type="submit" className="btn btn-block">
+					submit{" "}
+				</button>
+			</form>
+		</Wrapper>
+	);
 };
 
 export default Register;
